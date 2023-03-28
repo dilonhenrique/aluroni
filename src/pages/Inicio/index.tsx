463c2e75a94feeda077d98dbe539f1c025943656
dilonhenrique <dilonhenrique@gmail.com>
2023-03-28 11:12:52 -0300

    Teste versionamento

--- src/pages/Inicio/index.tsx
+++ src/pages/Inicio/index.tsx
@@ -3,7 +3,7 @@ import cardapio from 'pages/Cardapio/Itens/itens.json';
 export default function Inicio() {
     return (
         <section>
-            <h3>Recomendações da cozinha</h3>
+            <h3>Recomendações do Chef</h3>
             <div></div>
         </section>
     );
